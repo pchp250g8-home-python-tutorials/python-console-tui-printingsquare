f4300ca87b813b96cfd9ce5b2546f2e53d5f87ea
Alexander <pchp250g8@tutamail.com>
2026-01-28 12:47:16 +0300

    Created Project PrintingSquare

---
+++ PrintingSquare.py
@@ -0,0 +1,16 @@
+# --encoding:utf-8--
+import curses
+import time
+
+strLine = "Hello,World!!! Python Conio Example!!!"
+scr = curses.initscr()
+curses.curs_set(0)
+scr.clear()
+nLen = len(strLine)
+for i in range(0, nLen - 1):
+    scr.addstr(10, 10 + i, strLine[i] + chr(0x2588))
+    scr.refresh() 
+    time.sleep(.5) 
+scr.addstr(10, 10, strLine + " ")
+scr.getch()
+curses.endwin()
